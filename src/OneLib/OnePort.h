361c6bee72f06a3b7de6eca9480255d313815f29
Rui Azevedo <ruihfazevedo@gmail.com>
2017-11-02 05:46:37 -0100

    working

--- src/OneLib/OnePort.h
+++ src/OneLib/OnePort.h
@@ -1,63 +1,95 @@
 /* -*- C++ -*- */
+#include <avr/io.h>
 
 template<class T>
-struct Data {
-  T value;
-  inline Data(){}
-  inline Data(T o):value(o) {}
+struct Mem {
+  static inline T get(T* addr) {return *addr;}
+  static inline T set(T* addr,T v) {return *addr=v;}
 };
 
-typedef Data<uint8_t> Byte;
-typedef Data<uint16_t> Word;
+//void pin, use this if no pin is to be used
+struct VoidPin {
+  static inline void modeOut() {}
+  static inline void modeIn() {}
+  static inline void modeInUp() {}
+  static inline void on() {}
+  static inline void off() {}
+  static inline bool in() {return false;}
+  inline operator bool() {return in();}
+} voidPin;//or its objective version
 
-template<class T>
-using Ptr=Data<T*>;
-
-///////////////////
-template<class O,class T>
-struct DataInBase:public O {
-  using O::O;
-  inline T get() const {return O::value;}
-  inline bool operator==(T o) {return O::value==o;}
+template<class O,bool isOn>
+class LogicPinBase:public O {
+  public:
+    static inline bool in() {return O::in()^isOn;}
+    static inline void on() {isOn?O::off():O::on();}
+    static inline void off() {isOn?O::on():O::off();}
 };
-template<class O> using DataIn=DataInBase<O,typeof(O::value)>;
 
-template<class O,class T>
-struct DataOutBase:public O {
-  using O::O;
-  inline void set(T o) {O::value=o;}
-  inline T operator=(T o) {return O::value=o;}
-};
-template<class O> using DataOut=DataOutBase<O,typeof(O::value)>;
+//-------------------------------------------------------
+struct Avr {
 
-//the diamond problem with data
-template<class O,class T>
-struct DataIOBase:public DataIn<O>,public DataOut<O> {
-  using O::O;
-  inline T operator=(T o) {return DataOut<O>::operator=(o);}
-};
-template<class O> using DataIO=DataIOBase<O,typeof(O::value)>;
+  // hardwired code
+  template<uint8_t reg>
+  struct Reg:protected Mem<uint8_t> {
+    static inline uint8_t get() {return Mem::get(reg);}
+    static inline uint8_t set(uint8_t v) {return Mem::set(reg,v);}
+    template<uint8_t bit> static inline void on() {(*(uint8_t*)reg)|=1<<bit;}
+    template<uint8_t bit> static inline void off() {(*(uint8_t*)reg)&=~(1<<bit);}
+    template<uint8_t bit> static inline bool in() {return (*(uint8_t*)reg)&(1<<bit);}
+  };
 
-///////////////////
-template<class O,class T>
-struct RefInBase:public O {
-  using O::O;
-  inline T get() const {return *O::value;}
-  inline bool operator==(T o) {return get()==o;}
-};
-template<class O> using RefIn=RefInBase<O,typeof(O::value)>;
+  template<uint8_t base> using In = Avr::Reg<base>;
+  template<uint8_t base> using Mode = Avr::Reg<base+1>;
+  template<uint8_t base> using Out = Avr::Reg<base+2>;
 
-template<class O,class T>
-struct RefOutBase:public O {
-  using O::O;
-  inline void set(T o) {*O::value=o;}
-  inline T operator=(T o) {return *O::value=o;}
-};
-template<class O> using RefOut=RefOutBase<O,typeof(O::value)>;
+  template<uint8_t base>
+  struct Port:
+    protected In<base>,
+    protected Mode<base>,
+    protected Out<base>
+  {
+    static inline uint8_t mode() {return Mode<base>::get();}
+    static inline void mode(uint8_t m) {Mode<base>::set(m);}
+    static inline uint8_t in() {return In<base>::get();}
+    static inline void out(uint8_t data) {Out<base>::set(data);}
 
-template<class O,class T>
-struct RefIOBase:public RefIn<O>,public RefOut<O> {
-  using O::O;
-  inline T operator=(T o) {return DataOut<O>::operator=(o);}
+    template<uint8_t bit>
+    static inline void modeOut() {Mode<base>::template on<bit>();}
+    template<uint8_t bit>
+    static inline void modeIn() {
+      Mode<base>::template off<bit>();
+      Out<base>::template off<bit>();
+    }
+    template<uint8_t bit>
+    static inline void modeInUp() {
+      Mode<base>::template off<bit>();
+      Out<base>::template on<bit>();
+    }
+    template<uint8_t bit>
+    static inline void modeOff() {
+      Mode<base>::template off<bit>();
+      Out<base>::template off<bit>();
+    }
+    template<uint8_t bit>
+    static inline void on() {Out<base>::template on<bit>();}
+    template<uint8_t bit>
+    static inline void off() {Out<base>::template off<bit>();}
+
+    template<uint8_t bit>
+    static inline bool in() {return In<base>::template in<bit>();}
+
+  };
+  template<class Port,int pin>
+  struct PinBase:protected Port {
+    static inline void modeOut() {Port::template modeOut<pin>();}
+    static inline void modeIn() {Port::template modeIn<pin>();}
+    static inline void modeInUp() {Port::template modeInUp<pin>();}
+    static inline void on() {Port::template on<pin>();}
+    static inline void off() {Port::template off<pin>();}
+    static inline bool in() {return Port::template in<pin>();}
+    inline operator bool() {return in();}
+  };
+  template<class Port,int pin>
+  using Pin=LogicPinBase<PinBase<Port,pin<0?-pin:pin>,pin<0>;
 };
-template<class O> using RefIO=RefIOBase<O,typeof(O::value)>;
